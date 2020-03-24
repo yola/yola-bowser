82822dc8c4671c7866bbb124f63df9acc4c1abf9
ab <ytkonaft@gmail.com>
2020-03-24 14:28:58 +0200

    add test for proxy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.1.1
+
+- fix of `satisfies` method
+- add proxy `isSatisfied`
+
 ## 1.1.0
 
 - export `satisfies` method
---
+++ src/general-utils/is-satisfied.spec.js
@@ -0,0 +1,14 @@
+import isSatisfied from './is-satisfied';
+
+const getParserMock = () => ({
+  satisfies: jest.fn((params) => params)
+});
+
+describe('isSatisfied', () => {
+  it('should return true if condition is satisfied', () => {
+    const mock = getParserMock();
+    const params = 12;
+    expect(isSatisfied(mock, params)).toEqual(12);
+    expect(mock.satisfies).toHaveBeenCalled();
+  });
+});
